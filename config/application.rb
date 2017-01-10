0478a2f88f00f98e16ddcfe29106e50eb4783da2
Bethany Hyland <bethanyhyland29@gmail.com>
2017-01-09 20:15:53 -0500

    Testing

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module RememberTheMango
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
-        :s3_region => ENV["s3_region"]
+        :s3_region => ENV["AWS_REGION"]
       }
     }
     config.active_record.raise_in_transactional_callbacks = true
