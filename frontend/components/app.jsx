e247d30723f984cb090c2e6a23eee155236f2802
Bethany Hyland <bethanyhyland29@gmail.com>
2017-01-20 18:14:52 -0500

    Restructuring app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import SidebarContainer from './sidebar/sidebar_container';
 
-
-
 const App = ({ children }) => (
   <div className="home-page group">
     <GreetingContainer />
